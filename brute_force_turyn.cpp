0f67977caf4b46a6b305030a294221e9dca18d1a
Advait Sepuri <advaitsepuri@gmail.com>
2024-05-22 18:31:48 -0400

    brute force implementation

--- brute_force_turyn.cpp
+++ brute_force_turyn.cpp
@@ -1,14 +1,6 @@
 #include <iostream>
 #include <vector>
 
-// Function to implement the brute force turyn sequence algorithm
-void bruteForceSequence(int r) {
-    std::vector<int> W(r);
-    std::vector<int> X(r);
-    std::vector<int> Y(r);
-    std::vector<int> Z(r);
-}
-
 // Implementation of Non-periodic autocorrelation function(NPAF) with sequence X and shift s
 int NPAF(std::vector<int>& X, int s) {
     int sum = 0;
@@ -21,12 +13,77 @@ int NPAF(std::vector<int>& X, int s) {
     return sum;
 }
 
-int main() {
-    int r = 3; // odd length
+// check if 4 sequences are turyn sequences
+bool verifySequences(std::vector<int>& W, std::vector<int>& X,std::vector<int>& Y, std::vector<int>& Z, int r) {
+    if (W.size() != r || X.size() != r || Y.size() != r || Z.size() != r) {
+        throw std::invalid_argument("One of the sequences does not have a length equal to r");
+    }
+    std::vector<int> W_NPAF;
+    std::vector<int> X_NPAF;
+    std::vector<int> Y_NPAF;
+    std::vector<int> Z_NPAF;
+    for (int i = 1; i < r; i++) {
+        W_NPAF.push_back(NPAF(W, i));
+        X_NPAF.push_back(NPAF(X, i));
+        Y_NPAF.push_back(NPAF(Y, i));
+        Z_NPAF.push_back(NPAF(Z, i));
+    }
+    for (int i = 0; i < r - 1; i++) {
+        if (W_NPAF[i] + X_NPAF[i] + Y_NPAF[i] + Z_NPAF[i] != 0) {
+            return false;
+        }
+    }
+    return true;
+}
 
-    std::vector<int> X = {-1, -1, 1};
-    std::cout << NPAF(X, 1) << std::endl;
-    std::cout << NPAF(X, 2) << std::endl;
+// Function to implement the brute force turyn sequence algorithm
+int bruteForceTurynSequences(int r) {
+    int numSequences = 0;
+
+    std::vector<int> W(r);
+    std::vector<int> X(r);
+    std::vector<int> Y(r);
+    std::vector<int> Z(r);
+
+    for (int i = 0; i < (1 << r); ++i) {
+        for (int j = 0; j < r; ++j) {
+            W[j] = (i & (1 << j)) ? 1 : -1;
+        }
+        for (int i = 0; i < (1 << r); ++i) {
+            for (int j = 0; j < r; ++j) {
+                X[j] = (i & (1 << j)) ? 1 : -1;
+            }
+            for (int i = 0; i < (1 << r); ++i) {
+                for (int j = 0; j < r; ++j) {
+                    Y[j] = (i & (1 << j)) ? 1 : -1;
+                }
+                for (int i = 0; i < (1 << r); ++i) {
+                    for (int j = 0; j < r; ++j) {
+                        Z[j] = (i & (1 << j)) ? 1 : -1;
+                    }
+                    if (verifySequences(W, X, Y, Z, r)) {
+                        // std::cout << "W: ";
+                        // for (int w : W) std::cout << w << " ";
+                        // std::cout << "\nX: ";
+                        // for (int x : X) std::cout << x << " ";
+                        // std::cout << "\nY: ";
+                        // for (int y : Y) std::cout << y << " ";
+                        // std::cout << "\nZ: ";
+                        // for (int z : Z) std::cout << z << " ";
+                        // std::cout << "\n";
+                        numSequences++;
+                    }
+                }
+            }
+        }
+    }
+    return numSequences;
+}
+
+int main() {
+    int r = 5; // odd length
+
+    std::cout << bruteForceTurynSequences(r) << std::endl;
 
     return 0;
 }
