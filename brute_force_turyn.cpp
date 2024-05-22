93b5378e0041741a68a4b192d6e8812fcd40e912
Advait Sepuri <advaitsepuri@gmail.com>
2024-05-22 19:01:54 -0400

    sample implementation

--- brute_force_turyn.cpp
+++ brute_force_turyn.cpp
@@ -18,21 +18,36 @@ bool verifySequences(std::vector<int>& W, std::vector<int>& X,std::vector<int>& 
     if (W.size() != r || X.size() != r || Y.size() != r || Z.size() != r) {
         throw std::invalid_argument("One of the sequences does not have a length equal to r");
     }
+
     std::vector<int> W_NPAF;
     std::vector<int> X_NPAF;
     std::vector<int> Y_NPAF;
     std::vector<int> Z_NPAF;
+
     for (int i = 1; i < r; i++) {
         W_NPAF.push_back(NPAF(W, i));
         X_NPAF.push_back(NPAF(X, i));
         Y_NPAF.push_back(NPAF(Y, i));
         Z_NPAF.push_back(NPAF(Z, i));
     }
+
     for (int i = 0; i < r - 1; i++) {
         if (W_NPAF[i] + X_NPAF[i] + Y_NPAF[i] + Z_NPAF[i] != 0) {
             return false;
         }
     }
+
+    // std::cout << "NPAF-Applied Arrays:\n";
+    // std::cout << "W_NPAF: ";
+    // for (int w : W_NPAF) std::cout << w << " ";
+    // std::cout << "\nX_NPAF: ";
+    // for (int x : X_NPAF) std::cout << x << " ";
+    // std::cout << "\nY_NPAF: ";
+    // for (int y : Y_NPAF) std::cout << y << " ";
+    // std::cout << "\nZ_NPAF: ";
+    // for (int z : Z_NPAF) std::cout << z << " ";
+    // std::cout << "\n";
+
     return true;
 }
 
@@ -84,6 +99,13 @@ int main() {
     int r = 5; // odd length
 
     std::cout << bruteForceTurynSequences(r) << std::endl;
+
+    // Verifying sequences with r = 7
+    // std::vector<int> W = {1, 1, -1, -1, -1, -1, -1};
+    // std::vector<int> X = {1, -1, 1, 1, -1, 1, 1};
+    // std::vector<int> Y = {1, 1, 1, -1, -1, 1, 1};
+    // std::vector<int> Z = {1, -1, 1, -1, 1, -1, -1};
+    // verifySequences(W, X, Y, Z, 7);
 
     return 0;
 }
