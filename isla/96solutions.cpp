7d8a9abd15637fcd6b98b8093c2cb9487fd4b227
Advait Sepuri <advaitsepuri@gmail.com>
2024-05-26 21:42:02 -0400

    Brute Forcing Combinations

--- isla/96solutions.cpp
+++ isla/96solutions.cpp
@@ -3,16 +3,31 @@
 #include <fstream>
 #include "json.hpp"
 
+void bruteForcing96Solutions() {
+    int values[] = {-1, 1};
+    for (int w1 : values) {
+        for (int wr : values) {
+            for (int x1 : values) {
+                for (int xr : values) {
+                    for (int y1 : values) {
+                        for (int yr : values) {
+                            for (int z1 : values) {
+                                for (int zr : values) {
+                                    if (w1 * wr + x1 * xr + y1 * yr + z1 * zr == 0) {
+                                        // This combination satisfies the equation
+                                        std::cout << w1 << " " << wr << " " << x1 << " " << xr << " " << y1 << " " << yr << " " << z1 << " " << zr << std::endl;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
 int main() {
-    nlohmann::json j;
-    j["name"] = "John";
-    j["age"] = 30;
-    j["city"] = "New York";
-
-    std::ofstream o("person.json");
-    o << j << std::endl;
-
-    o.close();
-
+    bruteForcing96Solutions();
     return 0;
 }
